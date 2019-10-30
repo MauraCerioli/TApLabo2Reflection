1898c40541316d9c48d1c0c1650ce43aa3650178
Maura Cerioli <maura.cerioli@unige.it>
2019-10-30 14:33:52 +0100

    create solution structure add ExecuteMeAttribute

---
+++ MyAttribute/ExecuteMeAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAttribute
+{
+    [AttributeUsage(AttributeTargets.Method,AllowMultiple = true)]
+    public class ExecuteMeAttribute:Attribute{
+        public object[] Arguments{ get; private set; }
+        public ExecuteMeAttribute(params object[] arguments){
+            Arguments = arguments;
+        }
+    }
+}
