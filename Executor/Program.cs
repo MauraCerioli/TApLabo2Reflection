d7db5ebe91aacc094fd32cd74ca26feceabbcad8
Maura Cerioli <maura.cerioli@unige.it>
2019-10-30 14:46:09 +0100

    add MyLibrary load assembly in executor

--- Executor/Program.cs
+++ Executor/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Executor {
     class Program {
         static void Main(string[] args) {
+            var a = Assembly.LoadFrom("MyLibrary.dll");
+            foreach (var type in a.GetTypes())
+                if (type.IsClass)
+                    Console.WriteLine(type.FullName);
+            Console.ReadLine();
         }
     }
 }
