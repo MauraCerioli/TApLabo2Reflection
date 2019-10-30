c8a43c93058e529b9424c2a11e18c49b3ecb6562
Maura Cerioli <maura.cerioli@unige.it>
2019-10-30 14:58:17 +0100

    completed realease 1

--- Executor/Program.cs
+++ Executor/Program.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using MyAttribute;
 
-namespace Executor {
-    class Program {
-        static void Main(string[] args) {
+namespace Executor{
+    class Program{
+        static void Main(string[] args){
             var a = Assembly.LoadFrom("MyLibrary.dll");
             foreach (var type in a.GetTypes())
-                if (type.IsClass)
+                if (type.IsClass){
                     Console.WriteLine(type.FullName);
+                    var instance = Activator.CreateInstance(type);
+                    foreach (var m in type.GetMethods()){
+                        foreach (var att in m.GetCustomAttributes<ExecuteMeAttribute>()){
+                            m.Invoke(instance, att.Arguments);
+                        }
+                    }
+                }
+
             Console.ReadLine();
         }
     }
